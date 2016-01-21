3f1c73bac69d0d5e415a5258cb210cf7136cb0f8
Mary Scahill <mary@antarcti.cc>
2016-01-20 20:22:24 -0500

    Added variables to hold compWidth and compHeight. Added glPixelRead to grab value.

--- FFGL-SDK/Source/FFGLPlugins/FFGLMirror/FFGLMirror.cpp
+++ FFGL-SDK/Source/FFGLPlugins/FFGLMirror/FFGLMirror.cpp
@@ -2,6 +2,11 @@
 #include <FFGLLib.h>
 #include "FFGLMirror.h"
 
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //  Plugin information
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -34,6 +39,9 @@ FFGLMirror::FFGLMirror()
 	// No Parameters
 }
 
+
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //  Methods
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -47,7 +55,11 @@ DWORD FFGLMirror::ProcessOpenGL(ProcessOpenGLStruct *pGL)
     return FF_FAIL;
   
   FFGLTextureStruct &Texture = *(pGL->inputTextures[0]);
-
+  
+  int compWidth = Texture.Width;
+  int compHeight = Texture.Height;
+  
+  
   //bind the texture handle to its target
   glBindTexture(GL_TEXTURE_2D, Texture.Handle);
 
@@ -63,6 +75,8 @@ DWORD FFGLMirror::ProcessOpenGL(ProcessOpenGLStruct *pGL)
   glColor4f(1.f, 1.f, 1.f, 1.f);
   //(default texturemapping behavior of OpenGL is to
   //multiply texture colors by the current gl color)
+  
+
   
   //////////////////////////////////////////////
   //Draw the whole texture (video from resolume)
@@ -85,14 +99,47 @@ DWORD FFGLMirror::ProcessOpenGL(ProcessOpenGLStruct *pGL)
   glTexCoord2d(maxCoords.s, 0.0);
   glVertex2f(1,-1);
   glEnd();
-
-
+  
+  //////////////////////////////////////////////
+  //Get the pixel value at a point (0.5,0.5)
+  //////////////////////////////////////////////
+  GLfloat colorArray[3];
+  glReadPixels(compWidth*0.5,compHeight*0.5,1,1,GL_RGB,GL_FLOAT, colorArray);
 
   //unbind the texture
   glBindTexture(GL_TEXTURE_2D, 0);
-
+  
   //disable texturemapping
   glDisable(GL_TEXTURE_2D);
 
+
+  
+  //////////////////////////////////////////////
+  //Draw a square in the center with the color of the center of pixel
+  //////////////////////////////////////////////
+  glBegin(GL_QUADS);
+  
+  //lower left
+  glColor4f(colorArray[0], colorArray[1], colorArray[2], 1.f);
+  glVertex2f(-0.3,-0.3);
+  
+  //upper left
+  glVertex2f(-0.3,0.3);
+  
+  //upper right
+  glVertex2f(0.3,0.3);
+  
+  //lower right
+  glVertex2f(0.3,-0.3);
+  glEnd();
+  
+//  //unbind the texture
+//  glBindTexture(GL_TEXTURE_2D, 0);
+//  
+//  //disable texturemapping
+//  glDisable(GL_TEXTURE_2D);
+  
+
   return FF_SUCCESS;
 }
+
---
+++ FFGL-SDK/Source/FFGLPlugins/RGBtoHSV.hpp
@@ -0,0 +1,14 @@
+//
+//  RGBtoHSV.hpp
+//  FFGLPlugins
+//
+//  Created by Mary Scahill on 1/20/16.
+//
+//
+
+#ifndef RGBtoHSV_hpp
+#define RGBtoHSV_hpp
+
+#include <stdio.h>
+
+#endif /* RGBtoHSV_hpp */
